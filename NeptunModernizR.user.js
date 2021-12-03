54dd3f5a3bea973f26413888d14fd519e00178a3
Dávid Czompó <czompi@czompi.hu>
2021-12-03 11:32:26 +0100

    Update NeptunModernizR.user.js

--- NeptunModernizR.user.js
+++ NeptunModernizR.user.js
@@ -11,13 +11,13 @@
 // @include        https://host.sdakft.hu/*
 // @include        https://neptun.ejf.hu/ejfhw/*
 // @grant          none
-// @version        0.4
+// @version        0.5
 // @author         Czompi Software
 // @description    12/03/2021, 1:13:00 AM
 // @downloadURL    https://raw.githubusercontent.com/Czompi/NeptunModernizR/master/NeptunModernizR.user.js
 // ==/UserScript==
 
-const base_url = "https://cdn.jsdelivr.net/gh/Czompi/NeptunModernizR@0.4/";
+const base_url = "https://cdn.jsdelivr.net/gh/Czompi/NeptunModernizR@0.5/";
 
 var mastercss = document.createElement("link");
 mastercss.rel = "stylesheet";
