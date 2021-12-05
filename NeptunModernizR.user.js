22df674d5c92cce5f05267afcff7cf474a23a1b9
Dávid Czompó <czompi@czompi.hu>
2021-12-05 16:29:57 +0100

    Verziókezelés javítva

--- NeptunModernizR.user.js
+++ NeptunModernizR.user.js
@@ -10,23 +10,23 @@
 // @include        https://nappw.dfad.duf.hu/hallgato/*
 // @include        https://host.sdakft.hu/*
 // @include        https://neptun.ejf.hu/ejfhw/*
-// @grant          none
-// @version        0.11
+// @grant          GM.info
+// @version        0.12
 // @author         Czompi Software
 // @description    12/03/2021, 1:13:00 AM
 // @downloadURL    https://raw.githubusercontent.com/Czompi/NeptunModernizR/master/NeptunModernizR.user.js
 // ==/UserScript==
 
-const base_url = "https://cdn.jsdelivr.net/gh/Czompi/NeptunModernizR@0.11/dist/";
+const base_url = `https://cdn.jsdelivr.net/gh/Czompi/NeptunModernizR@${GM.info.script.version}/dist/`;
 
 var mastercss = document.createElement("link");
 mastercss.rel = "stylesheet";
 mastercss.type = "text/css";
-mastercss.href = base_url + "master.css";
+mastercss.href = `${base_url}master.css`;
 var masterdarkcss = document.createElement("link");
 masterdarkcss.rel = "stylesheet";
 masterdarkcss.type = "text/css";
-masterdarkcss.href = base_url + "master-dark.css";
+masterdarkcss.href = `${base_url}master-dark.css`;
 
 function
 init() {
