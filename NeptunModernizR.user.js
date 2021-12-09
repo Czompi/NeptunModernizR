a9b98450aa1a057f02549a91aa2124172ff20a34
Dávid Czompó <czompi@czompi.hu>
2021-12-09 22:57:19 +0100

    A lot of changes #1
    - Fixed checkbox indeterm. state & added, captions tooltips & contextdivs
    - Added table support

--- NeptunModernizR.user.js
+++ NeptunModernizR.user.js
@@ -11,7 +11,7 @@
 // @include        https://host.sdakft.hu/*
 // @include        https://neptun.ejf.hu/ejfhw/*
 // @grant          GM.info
-// @version        0.13
+// @version        0.14
 // @author         Czompi Software
 // @description    12/03/2021, 1:13:00 AM
 // @downloadURL    https://raw.githubusercontent.com/Czompi/NeptunModernizR/master/NeptunModernizR.user.js
