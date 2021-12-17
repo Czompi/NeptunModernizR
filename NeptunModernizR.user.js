556bea1a8ba91da3c445d891dac90e2816276785
Dávid Czompó <czompi@czompi.hu>
2021-12-17 02:29:30 +0100

    Added border to color-box, dropdown active tag and fixed dropdown

--- NeptunModernizR.user.js
+++ NeptunModernizR.user.js
@@ -11,7 +11,7 @@
 // @include        https://host.sdakft.hu/*
 // @include        https://neptun.ejf.hu/ejfhw/*
 // @grant          GM.info
-// @version        0.38
+// @version        0.39
 // @author         Czompi Software
 // @description    12/03/2021, 1:13:00 AM
 // @downloadURL    https://raw.githubusercontent.com/Czompi/NeptunModernizR/master/NeptunModernizR.user.js
@@ -55,29 +55,30 @@ function getCurrentLang() {
   return (help_link.href || 'help/hweb_de.pdf').match(/(?<=help\/hweb_)\S+(?=\.pdf)/)[0];
 }
 function getLangSelector() {
+	var ct = getCurrentTheme();
     $(".top_menu_right").empty();
     $(".top_menu_right").append("<div class=\"btn-group\">\r\n\t" +
         "<button class=\"btn btn-secondary btn-sm dropdown-toggle\" type=\"button\" id=\"dropdownMenuButton1\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">Small button</button>"+
         "<ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton1\">\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_Pink');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "pink" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_Pink');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #E20074;\"></span> Pink\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_Gfx');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "blue" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_Gfx');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #3B49C9;\"></span> Blue\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_Lime');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "lime" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_Lime');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #8FCC00;\"></span> Lime\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_Orange');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "orange" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_Orange');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #FBA04B;\"></span> Orange\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_Classic');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "brown" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_Classic');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #783F04;\"></span> Brown\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_NewYork');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "tomato" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_NewYork');\">\r\n\t\t\t"+
             	"<span class=\"color-box\" style=\"--color: #FF6347;\"></span> Tomato\r\n\t\t"+
             "</a></li>\r\n\t\t" +
-            "<li><a class=\"dropdown-item\" onclick=\"javascript:SkinChoose('Skin_Neptun_PartiallySighted');\">\r\n\t\t\t"+
+            "<li><a class=\"dropdown-item" + (ct.toLowerCase() == "partiallysighted" ? " active" : "") + "\" onclick=\"javascript:SkinChoose('Skin_Neptun_PartiallySighted');\">\r\n\t\t\t"+
 				"<span class=\"color-box\" style=\"--color: linear-gradient(135deg, rgba(2,0,36,1) 0%, rgba(255,201,0,1) 100%);\"></span> Partially sighted\r\n\t\t"+
 			"</a></li>\r\n\t\t" +
         "</ul>\r\n\t" +
