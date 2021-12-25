0c92c66731ba3fb20f3fb7aec0589ad3ccd045af
Dávid Czompó <czompi@czompi.hu>
2021-12-25 12:10:46 +0100

    Fixed current day color

--- NeptunModernizR.user.js
+++ NeptunModernizR.user.js
@@ -12,7 +12,7 @@
 // @include        https://neptun.ejf.hu/ejfhw/*
 // @include        https://neptun.ejf.hu/ejfow/*
 // @grant          GM.info
-// @version        0.47
+// @version        0.48
 // @author         Czompi Software
 // @description    12/03/2021, 1:13:00 AM
 // @downloadURL    https://raw.githubusercontent.com/Czompi/NeptunModernizR/master/NeptunModernizR.user.js
